a523452b33e42d29236ffb42f0566b01cf4cdfc6
coclyun <coclyun@gmail.com>
2025-06-07 15:52:53 +0800

    优化 切换移动网络的重连逻辑

--- lib/app/services/socket_service.dart
+++ lib/app/services/socket_service.dart
@@ -26,6 +26,7 @@ import 'package:clipshare/app/utils/constants.dart';
 import 'package:clipshare/app/utils/crypto.dart';
 import 'package:clipshare/app/utils/extensions/platform_extension.dart';
 import 'package:clipshare/app/utils/extensions/string_extension.dart';
+import 'package:clipshare/app/utils/extensions/time_extension.dart';
 import 'package:clipshare/app/utils/global.dart';
 import 'package:clipshare/app/utils/log.dart';
 import 'package:collection/collection.dart';
@@ -84,7 +85,7 @@ class DevSocket {
   bool isPaired;
   AppVersion? minVersion;
   AppVersion? version;
-  DateTime? lastPingTime;
+  DateTime lastPingTime = DateTime.now();
 
   DevSocket({
     required this.dev,
@@ -569,8 +570,19 @@ class SocketService extends GetxService with ScreenOpenedObserver {
     var address = ipSetTemp.firstWhereOrNull((ip) => ip.split(":")[0] == client.ip);
     switch (msg.key) {
       case MsgType.ping:
+        var skt = _devSockets[dev.guid];
         if (_devSockets.containsKey(dev.guid)) {
-          _devSockets[dev.guid]!.updatePingTime();
+          skt!.updatePingTime();
+          if (msg.data.containsKey("result")) {
+            sendData(dev, MsgType.pingResult, {});
+          }
+        }
+        break;
+
+      case MsgType.pingResult:
+        var skt = _devSockets[dev.guid];
+        if (_devSockets.containsKey(dev.guid)) {
+          skt!.updatePingTime();
         }
         break;
 
@@ -1005,12 +1017,15 @@ class SocketService extends GetxService with ScreenOpenedObserver {
 
     for (var dev in devices) {
       var [ip, port] = dev.address!.split(":");
-      //检测当前网络环境,如果不是 WiFi 且设备是中转地址，直接连接中转而不是走完整设备发现流程
-      if (forwardIp == ip) {
-        if (!isWifi && PlatformExt.isMobile && forwardServerPort != null) {
+      //检测当前网络环境，以下条件直接直接连接中转，而不是走完整设备发现流程
+      //1. 不是 WiFi 且为移动设备
+      //2. 不是 WiFi 且地址为中转地址
+      if (!isWifi) {
+        if (PlatformExt.isMobile || forwardIp == ip) {
+          print("connect by forward ${dev.name}(${dev.guid})");
           tasks.add(() => manualConnectByForward(dev.guid));
+          continue;
         }
-        continue;
       }
       tasks.add(() => manualConnect(ip, port: int.parse(port)));
     }
@@ -1030,8 +1045,41 @@ class SocketService extends GetxService with ScreenOpenedObserver {
     return tasks;
   }
 
+  ///检查是否已经掉线，如果掉线则移除
+  Future<bool> testIsOnline(String devId) async {
+    if (!_devSockets.containsKey(devId)) return false;
+    var skt = _devSockets[devId]!;
+    //发送一个ping事件，但是要求对方给回复
+    await sendData(skt.dev, MsgType.ping, {
+      "result": null,
+    });
+    print("testIsOnline: send ping result");
+    //等待500ms
+    const waitTime = Duration(milliseconds: 500);
+    await Future.delayed(waitTime);
+    print("testIsOnline: waitTime finished");
+    //等待过程中已经掉线
+    if (_devSockets.containsKey(devId)) {
+      print("testIsOnline: offline in waitTime");
+      _onDevDisconnected(devId);
+      return false;
+    }
+    skt = _devSockets[devId]!;
+    //检查上次ping的时间是否在误差范围内，如果不在这个范围说明可能已经掉线
+    final online = skt.lastPingTime.isWithinRange(waitTime);
+    print("testIsOnline: isWithinRange $online");
+    if (!online) {
+      _onDevDisconnected(devId);
+    }
+    return online;
+  }
+
   ///中转连接设备
-  Future<bool> manualConnectByForward(String devId) {
+  Future<bool> manualConnectByForward(String devId) async {
+    if (await testIsOnline(devId)) {
+      Log.debug(tag, "dev($devId) online, cancel connect by forward");
+      return false;
+    }
     return manualConnect(
       forwardServerHost!,
       port: forwardServerPort,
@@ -1370,10 +1418,7 @@ class SocketService extends GetxService with ScreenOpenedObserver {
     final now = DateTime.now();
     var skts = _devSockets.values.toList();
     for (var ds in skts) {
-      if (ds.lastPingTime == null) {
-        continue;
-      }
-      final diff = now.difference(ds.lastPingTime!);
+      final diff = now.difference(ds.lastPingTime);
       if (diff.inSeconds > interval) {
         //心跳超时
         Log.debug(tag, "judgeDeviceHeartbeatTimeout ${ds.dev.guid}");
