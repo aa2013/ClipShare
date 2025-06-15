f9e4b4e7b12f88b61ae44599108c3a8621950019
coclyun <2439827731@qq.com>
2025-06-15 17:34:43 +0800

    update logs

--- lib/app/modules/update_log_module/update_log_page.dart
+++ lib/app/modules/update_log_module/update_log_page.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:clipshare/app/data/enums/translation_key.dart';
 import 'package:clipshare/app/modules/update_log_module/update_log_controller.dart';
+import 'package:clipshare/app/utils/extensions/string_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
@@ -14,7 +17,7 @@ class UpdateLogPage extends GetView<UpdateLogController> {
     return Scaffold(
       appBar: AppBar(title: Text(TranslationKey.updateLogPageAppBarTitle.tr)),
       body: FutureBuilder(
-        future: rootBundle.loadString("assets/md/updateLogs.md"),
+        future: rootBundle.loadString("assets/md/updateLogs-${Platform.operatingSystem.upperFirst()}.md"),
         builder: (context, v) {
           return Markdown(
             data: v.data ?? TranslationKey.failedToReadUpdateLog.tr,
--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,7 +6,7 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <clipshare_clipboard_listener/clipboard_listener_plugin.h>
+#include <clipshare_clipboard_listener/clipshare_clipboard_listener_plugin.h>
 #include <desktop_drop/desktop_drop_plugin.h>
 #include <desktop_multi_window/desktop_multi_window_plugin.h>
 #include <hotkey_manager_linux/hotkey_manager_linux_plugin.h>
@@ -19,8 +19,8 @@
 
 void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) clipshare_clipboard_listener_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "ClipboardListenerPlugin");
-  clipboard_listener_plugin_register_with_registrar(clipshare_clipboard_listener_registrar);
+      fl_plugin_registry_get_registrar_for_plugin(registry, "ClipshareClipboardListenerPlugin");
+  clipshare_clipboard_listener_plugin_register_with_registrar(clipshare_clipboard_listener_registrar);
   g_autoptr(FlPluginRegistrar) desktop_drop_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "DesktopDropPlugin");
   desktop_drop_plugin_register_with_registrar(desktop_drop_registrar);
