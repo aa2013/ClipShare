fa579904512956edb514f6ffd76c7194b3fc497e
coclyun <2439827731@qq.com>
2026-01-19 23:04:45 +0800

    linux 打包

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,2 +1,12 @@
-flutter build linux --release
-xdg-open ../build/linux/x64/release/bundle
+# https://fastforge.dev/zh/getting-started
+# 1. dart pub global activate fastforge # fastforge 配置环境变量
+# 2. appimage:
+# 2.1 sudo apt install locate
+# 2.2 wget -O appimagetool "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+#      chmod +x appimagetool
+#      sudo mv appimagetool /usr/local/bin/
+# 3. rpm
+# 3.1 Debian/Ubuntu: apt install rpm patchelf
+# 3.2 Fedora: dnf install gcc rpm-build rpm-devel rpmlint make python bash coreutils diffutils patch rpmdevtools patchelf
+# 3.3 Arch: yay -S rpmdevtools patchelf or pamac install rpmdevtools patchelf
+cd ../ && fastforge package --platform linux --targets deb,appimage,rpm
