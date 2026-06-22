5a64b372cc9675f145231cd54c468c76db15eaf0
coclyun <2439827731@qq.com>
2026-06-22 16:03:16 +0800

    优化 丢弃重复的通知内容

--- lib/app/services/android_notification_listener_service.dart
+++ lib/app/services/android_notification_listener_service.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:clipshare/app/data/enums/history_content_type.dart';
 import 'package:clipshare/app/listeners/history_data_listener.dart';
 import 'package:clipshare/app/services/clipboard_source_service.dart';
+import 'package:clipshare/app/utils/extensions/number_extension.dart';
 import 'package:clipshare/app/utils/extensions/string_extension.dart';
 import 'package:clipshare/app/utils/log.dart';
 import 'package:clipshare_clipboard_listener/models/clipboard_source.dart';
@@ -16,6 +17,7 @@ class AndroidNotificationListenerService extends GetxService {
   StreamSubscription<ServiceNotificationEvent>? _listen;
   final _sourceService = Get.find<ClipboardSourceService>();
   var _listening = false;
+  final _notifyHashSet = <String>{};
 
   bool get listening => _listening;
 
@@ -26,10 +28,6 @@ class AndroidNotificationListenerService extends GetxService {
     _listen?.cancel();
     _listening = false;
     _listen = NotificationListenerService.notificationsStream.listen(_onNotifyEvent);
-    _listen?.onDone(() {
-      logger.info(tag, "on done");
-      _listening = false;
-    });
     _listening = true;
   }
 
@@ -45,14 +43,25 @@ class AndroidNotificationListenerService extends GetxService {
       if (event.hasRemoved == true) {
         return;
       }
-      var map = <String, String?>{};
-      final hasImg = event.haveExtraPicture ?? false;
-      map["pkg"] = event.packageName;
-      map["title"] = event.title;
-      map["content"] = event.content;
-      if (event.content?.isNullOrEmpty ?? true) {
+      final pkg = event.packageName;
+      final title = event.title;
+      final content = event.content;
+      if (content?.isNullOrEmpty ?? true) {
         return;
       }
+      final hash = "$pkg$title$content".toMd5();
+      if (!_notifyHashSet.add(hash)) {
+        //duplicate content
+        return;
+      }
+      Future.delayed(2.s, () => _notifyHashSet.remove(hash));
+      var map = <String, String?>{};
+      final hasImg = event.haveExtraPicture ?? false;
+
+      map["pkg"] = pkg;
+      map["title"] = title;
+      map["content"] = content;
+
       if (hasImg) {
         try {
           map["img"] = base64Encode(event.extrasPicture!);
